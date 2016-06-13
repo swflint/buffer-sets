2d2863b330177cc6fd1ffb5711b0e89469e431e8
Samuel W. Flint <swflint@flintfam.org>
2016-06-12 19:53:47 -0500

    Added Commentary

--- buffer-layers.el
+++ buffer-layers.el
@@ -10,6 +10,15 @@
 
 ;;; Commentary:
 ;;
+;; This is Buffer Layers, a simple, layer-based buffer management system.
+;;
+;; It works by defining buffer layers using a fairly simple macro.  An example is as follows:
+;;
+;; (define-buffer-layer org
+;;   :files ("~/org/"
+;;           "~/org/main.org")
+;;   :buffer-to-select "main.org"
+;;   :run-on-apply ((my/find-current-notes-file)))
 
 
 ;;; Code:
